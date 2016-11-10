de5ec40dcc4c9cbf620afec252fa8c1766401262
Daniel Williams (Insight Global) <v-wildan@microsoft.com>
2016-11-09 17:04:30 -0800

    Initial commit

---
+++ spa.wrapper/Properties/PublishProfiles/Release-publish.ps1
@@ -0,0 +1,20 @@
+﻿[cmdletbinding(SupportsShouldProcess=$true)]
+param($publishProperties=@{}, $packOutput, $pubProfilePath)
+
+'*** Publish properties as json' | Write-Output
+$publishProperties | ConvertTo-Json -Depth 4 | Write-Output
+
+try{
+    if ($publishProperties['ProjectGuid'] -eq $null){
+        $publishProperties['ProjectGuid'] = 'ce8d4432-5eb0-4d26-8ff8-2aeac9c1d814'
+    }
+
+    $publishModulePath = Join-Path (Split-Path $MyInvocation.MyCommand.Path) 'publish-module.psm1'
+    Import-Module $publishModulePath -DisableNameChecking -Force
+
+    # call Publish-AspNet to perform the publish operation
+    Publish-AspNet -publishProperties $publishProperties -packOutput $packOutput -pubProfilePath $pubProfilePath
+}
+catch{
+    "An error occurred during publish.`n{0}" -f $_.Exception.Message | Write-Error
+}
